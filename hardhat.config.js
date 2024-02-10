e30fabfd9b33cd801c18e92d251b614ac63bbfdb
Aaron Gebremaraim <mulabr673@gmail.com>
2024-02-10 15:52:46 +0300

    update .config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    localhost: {
+      url: "http://localhost:7545", // Use the default Hardhat network URL
+      accounts: [
+        "0x5099112334b72bb370da733ce5e174af25c307cd"
+      ]
+    }
+  }
 };
